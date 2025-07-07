d618f12466fa720d2b1aa71389663ff70c4d30ca
EugeneKulagin <kulagines@gmail.com>
2025-07-07 09:18:59 +0300

    Initial commit

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -1,5 +1,6 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,77 +12,95 @@ import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.security.Principal;
 
-@Controller
-@RequestMapping("/admin")
-public class AdminController {
+    @Controller
+    @RequestMapping("/admin")
+    public class AdminController {
 
-    private final UserService userService;
-    private final RoleService roleService;
+        private final UserService userService;
+        private final RoleService roleService;
 
-    public AdminController(UserService userService, RoleService roleService) {
-        this.userService = userService;
-        this.roleService = roleService;
-    }
-
-    @GetMapping
-    public String showUserList(
-            @RequestParam(value = "editId", required = false) Long editId,
-            @RequestParam(value = "deleteId", required = false) Long deleteId,
-            Model model,
-            Principal principal) {
-
-        User authUser = userService.findUserByUsername(principal.getName());
-        model.addAttribute("authUser", authUser);
-        model.addAttribute("users", userService.findAll());
-        model.addAttribute("allRoles", roleService.getRoles());
-        if (editId != null) {
-            User editUser = userService.findById(editId);
-            model.addAttribute("editUser", editUser);
+        public AdminController(UserService userService, RoleService roleService) {
+            this.userService = userService;
+            this.roleService = roleService;
         }
-        if (deleteId != null) {
-            User deleteUser = userService.findById(deleteId);
-            model.addAttribute("deleteUser", deleteUser);
+
+        @GetMapping
+        public String showUserList(
+                @RequestParam(value = "editId", required = false) Long editId,
+                @RequestParam(value = "deleteId", required = false) Long deleteId,
+                Model model,
+                Principal principal) {
+
+            User authUser = userService.findUserByUsername(principal.getName());
+            model.addAttribute("authUser", authUser);
+            model.addAttribute("users", userService.findAll());
+            model.addAttribute("allRoles", roleService.getRoles());
+            if (editId != null) {
+                User editUser = userService.findById(editId);
+                model.addAttribute("editUser", editUser);
+            }
+            if (deleteId != null) {
+                User deleteUser = userService.findById(deleteId);
+                model.addAttribute("deleteUser", deleteUser);
+            }
+            return "admin";
         }
-        return "admin";
-    }
 
-    @GetMapping("/new")
-    public String createUser(Model model, Principal principal) {
-        User authUser = userService.findUserByUsername(principal.getName());
-        model.addAttribute("authUser", authUser);
-        model.addAttribute("user", new User());
-        model.addAttribute("allRoles", roleService.getRoles());
-        return "new";
-    }
-
-    @PostMapping("/new")
-    public String createUser(@ModelAttribute("user") @Validated User user, BindingResult result) {
-        if (result.hasErrors()) {
+        @GetMapping("/new")
+        public String createUser(Model model, Principal principal) {
+            User authUser = userService.findUserByUsername(principal.getName());
+            model.addAttribute("authUser", authUser);
+            model.addAttribute("user", new User());
+            model.addAttribute("allRoles", roleService.getRoles());
             return "new";
         }
-        userService.save(user);
-        return "redirect:/admin";
-    }
 
-    @GetMapping("/edit/{id}")
-    public String editUser(@PathVariable("id") Long id, Model model, Principal principal) {
-        User authUser = userService.findUserByUsername(principal.getName());
-        model.addAttribute("authUser", authUser);
-        model.addAttribute("user", userService.findById(id));
-        model.addAttribute("users", userService.findAll());
-        model.addAttribute("allRoles", roleService.getRoles());
-        return "admin";
-    }
+        @PostMapping("/new")
+        public String createUser(@ModelAttribute("user") @Validated User user, BindingResult result) {
+            if (result.hasErrors()) {
+                return "new";
+            }
+            userService.save(user);
+            return "redirect:/admin";
+        }
 
-    @PostMapping("/update")
-    public String updateUser(@ModelAttribute("editUser") User user) {
-        userService.update(user);
-        return "redirect:/admin";
-    }
+        @GetMapping("/edit/{id}")
+        public String editUser(@PathVariable("id") Long id, Model model, Principal principal) {
+            User authUser = userService.findUserByUsername(principal.getName());
+            model.addAttribute("authUser", authUser);
+            model.addAttribute("user", userService.findById(id));
+            model.addAttribute("users", userService.findAll());
+            model.addAttribute("allRoles", roleService.getRoles());
+            return "admin";
+        }
 
-    @PostMapping("/delete/{id}")
-    public String deleteUser(@PathVariable("id") Long id) {
-        userService.delete(id);
-        return "redirect:/admin";
+        @PostMapping("/update")
+        public String updateUser(@ModelAttribute("editUser") User user) {
+            userService.update(user);
+            return "redirect:/admin";
+        }
+
+        @PostMapping("/delete/{id}")
+        public String deleteUser(@PathVariable("id") Long id) {
+            userService.delete(id);
+            return "redirect:/admin";
+        }
+    }
+@Controller
+class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public String home(Principal principal) {
+        if (principal != null) {
+            User user = userService.findUserByUsername(principal.getName());
+            if (user.getRoles().size() == 2) {
+                return "redirect:/admin";
+            }
+        }
+        return "home";
     }
 }
+
--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.entityes.User;
@@ -16,15 +18,19 @@ import java.util.List;
 public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     @Override
     @Transactional
     public void save(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         userRepository.save(user);
     }
 
@@ -41,6 +47,10 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     @Override
     @Transactional
     public void update(User user) {
+        if (user.getPassword() != null && !user.getPassword().isEmpty()) {
+            String encodedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(encodedPassword);
+        }
         userRepository.save(user);
     }
 
